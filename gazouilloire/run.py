8f83efb3e0e1b3a554c93a0d25fee307078c3c49
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-02-07 18:14:10 +0100

    first basic tool

---
+++ gazouilloire/run.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, time, re, urllib, json
+import pymongo
+from multiprocessing import Process, Queue
+from twitter import Twitter, TwitterStream, OAuth, OAuth2
+from tweets import prepare_tweets
+
+def depiler(pile, db):
+    while True:
+        todo = []
+        while not pile.empty():
+            todo.append(pile.get())
+        save = prepare_tweets(todo)
+        for t in save:
+            tid = db.save(t)
+    #        sys.stderr.write("DEBUG: saved tweet %s\n" % tid)
+
+def streamer(pile, streamco, keywords):
+    while True:
+        for msg in streamco.statuses.filter(track=",".join([k.lstrip('@').strip().lower() for k in keywords]).encode('utf-8'), filter_level='none', stall_warnings='true'):
+            print "TEST"
+            if not msg:
+                continue
+            if msg.get("disconnect") or msg.get("timeout") or msg.get("hangup"):
+                sys.stderrr.write("Stream connection lost, restarting it: %s\n" % msg)
+                break
+            if msg.get('text'):
+                pile.put(dict(msg))
+            else:
+                sys.stderrr.write("Got special data:\n")
+                sys.stderrr.write(str(msg))
+
+def searcher(pile, searchco, keywords):
+    ts = 0
+    since_id = 0
+    query = " OR ".join([urllib.quote(k.encode('utf-8').replace('@', 'from:'),'') for k in conf['keywords']])
+    while True:
+        if time.time() - ts > 15*60:
+            print "RESET TIME"
+            ts = time.time()
+            left = 450
+        if not left:
+            print "SLEEP"
+            time.sleep(ts + 15*60 - time.time())
+            continue
+        print "SEARCH!"
+        max_id = 0
+        since = since_id
+        while left:
+            args = {'q': query, 'count': 100, 'include_entities': True}
+            if max_id:
+                args['max_id'] = str(max_id)
+            if since_id:
+                args['since_id'] = str(since_id)
+            print "QUERY", args
+            res = searchco.search.tweets(**args)
+            metas = res.get('search_metadata', {})
+            tweets = res.get('statuses', [])
+            print "RES", len(tweets)
+            if not len(tweets):
+                break
+            for tw in tweets:
+                tid = long(tw.get('id_str', str(tw.get('id', ''))))
+                if not tid:
+                    continue
+                if since < tid:
+                    since = tid + 1
+                if not max_id or max_id > tid:
+                    max_id = tid - 1
+                pile.put(dict(tw))
+        since_id = since
+        print "SLEEP"
+        time.sleep(5)
+
+if __name__=='__main__':
+    try:
+        with open('config.json') as confile:
+            conf = json.loads(confile.read())
+        oauth = OAuth(conf['twitter']['oauth_token'], conf['twitter']['oauth_secret'], conf['twitter']['key'], conf['twitter']['secret'])
+        oauth2 = OAuth2(bearer_token=json.loads(Twitter(api_version=None, format="", secure=True, auth=OAuth2(conf['twitter']['key'], conf['twitter']['secret'])).oauth2.token(grant_type="client_credentials"))['access_token'])
+        SearchConn = Twitter(domain="api.twitter.com", api_version="1.1", format="json", auth=oauth2, secure=True)
+        StreamConn = TwitterStream(domain="stream.twitter.com", api_version="1.1", auth=oauth, secure=True)
+    except Exception as e:
+        print type(e), e
+        sys.stderr.write('ERROR: Could not initiate connections to Twitter API\n')
+        sys.exit(1)
+    try:
+        db = pymongo.Connection(conf['mongo']['host'], conf['mongo']['port'])[conf['mongo']['db']]
+        coll = db['tweets']
+        coll.ensure_index([('_id', pymongo.ASCENDING)], background=True)
+        coll.ensure_index([('timestamp', pymongo.ASCENDING)], background=True)
+    except:
+        sys.stderr.write('ERROR: Could not initiate connection to MongoDB\n')
+        sys.exit(1)
+
+    pile = Queue()
+    depile = Process(target=depiler, args=((pile), coll,))
+    depile.daemon = True
+    depile.start()
+    stream = Process(target=streamer, args=((pile), StreamConn, conf['keywords']))
+    stream.daemon = True
+    stream.start()
+    search = Process(target=searcher, args=((pile), SearchConn, conf['keywords']))
+    search.start()
+    depile.join()
+
---
+++ gazouilloire/tweets.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re, htmlentitydefs
+
+re_entities = re.compile(r'&([^;]+);')
+def unescape_html(text):
+    return re_entities.sub(lambda x: unichr(int(x.group(1)[1:])) if x.group(1).startswith('#') else unichr(htmlentitydefs.name2codepoint[x.group(1)]), text)
+
+def grab_extra_meta(source, result):
+    for meta in ["in_reply_to_status_id_str", "in_reply_to_screen_name", "lang", "geo", "coordinates", "source"]:
+        if meta in source:
+            result[meta] = source[meta]
+    for meta in ['name', 'friends_count', 'followers_count', 'statuses_count', 'listed_count']:
+        key = "user_%s" % meta.replace('_count', '')
+        if key in source:
+            result[key] = source[key]
+        elif 'user' in source and meta in source['user']:
+            result[key] = source['user'][meta]
+    return result
+
+def prepare_tweets(tweets):
+    tosave = []
+    for tweet in tweets:
+        if not isinstance(tweet, dict):
+            continue
+        if "retweeted_status" in tweet and tweet['retweeted_status']['id_str'] != tweet['id_str']:
+            text = "RT @%s: %s" % (tweet['retweeted_status']['user']['screen_name'], tweet['retweeted_status']['text'])
+        else:
+            text = tweet['text']
+        if 'entities' in tweet:
+            for entity in tweet['entities'].get('media', []) + tweet['entities'].get('urls', []):
+                if 'expanded_url' in entity and 'url' in entity and entity['expanded_url']:
+                    try:
+                        text = text.replace(entity['url'], entity['expanded_url'])
+                    except:
+                        pass
+        tw = {'_id': tweet['id_str'],
+              'created_at': tweet['created_at'],
+              'text': unescape_html(text),
+              'url': "https://twitter.com/%s/statuses/%s" % (tweet['user']['screen_name'], tweet['id_str'])}
+        tw = grab_extra_meta(tweet, tw)
+        tosave.append(tw)
+    return tosave
+
+
