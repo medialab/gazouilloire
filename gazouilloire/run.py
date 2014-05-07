feb4d39b6756021f22c508e006714d1e6942bd2e
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-05-07 13:17:00 +0200

    fix logs

--- gazouilloire/run.py
+++ gazouilloire/run.py
@@ -12,7 +12,7 @@ from twitter import Twitter, TwitterStream, OAuth, OAuth2, TwitterHTTPError
 from tweets import prepare_tweets
 
 def log(typelog, text):
-    sys.stderr.write("[%s] %s: %s" % (datetime.now(), typelog, text))
+    sys.stderr.write("[%s] %s: %s\n" % (datetime.now(), typelog, text))
 
 def depiler(pile, db, debug=False):
     while True:
@@ -23,17 +23,17 @@ def depiler(pile, db, debug=False):
         for t in save:
              tid = db.save(t)
         if debug and save:
-            log("DEBUG", "Saved %s tweets in MongoDB\n" % len(save))
+            log("DEBUG", "Saved %s tweets in MongoDB" % len(save))
         time.sleep(2)
 
 def streamer(pile, streamco, keywords, debug=False):
     while True:
         ts = time.time()
-        log('INFO', 'Starting stream track\n')
+        log('INFO', 'Starting stream track')
         try:
             streamiter = streamco.statuses.filter(track=",".join([k.lstrip('@').strip().lower() for k in keywords]).encode('utf-8'), filter_level='none', stall_warnings='true')
         except (TwitterHTTPError, BadStatusLine, URLError, SSLError) as e:
-            log("WARNING", "Stream connection could not be established, retrying in 2 secs (%s: %s)\n" % (type(e), e))
+            log("WARNING", "Stream connection could not be established, retrying in 2 secs (%s: %s)" % (type(e), e))
             time.sleep(2)
             continue
         try:
@@ -41,20 +41,20 @@ def streamer(pile, streamco, keywords, debug=False):
                 if not msg:
                     continue
                 if msg.get("disconnect") or msg.get("hangup"):
-                    log("WARNING", "Stream connection lost: %s\n" % msg)
+                    log("WARNING", "Stream connection lost: %s" % msg)
                     break
                 if msg.get("timeout"):
                     continue
                 if msg.get('text'):
                     pile.put(dict(msg))
                     if debug:
-                        log("DEBUG", "[stream] +1 tweet\n")
+                        log("DEBUG", "[stream] +1 tweet")
                 else:
-                    log("INFO", "Got special data: %s\n" % str(msg))
+                    log("INFO", "Got special data: %s" % str(msg))
         except (TwitterHTTPError, BadStatusLine, URLError, SSLError, socket.error) as e:
-            log("WARNING", "Stream connection lost, reconnecting in a sec... (%s: %s)\n" % (type(e), e))
+            log("WARNING", "Stream connection lost, reconnecting in a sec... (%s: %s)" % (type(e), e))
         if debug:
-            log("DEBUG", "Stream stayed alive for %ss" % str(time.time()-ts))
+            log("DEBUG", "Stream stayed alive for %sh" % str((time.time()-ts)/3600))
         time.sleep(2)
 
 chunkize = lambda a, n: [a[i:i+n] for i in xrange(0, len(a), n)]
@@ -67,7 +67,7 @@ def searcher(pile, searchco, keywords, debug=False):
     try:
         next_reset, max_per_reset, left = get_twitter_rates(searchco)
     except:
-        log("ERROR", "Connecting to Twitter API via OAuth2 sign, could not get rate limits\n")
+        log("ERROR", "Connecting to Twitter API via OAuth2 sign, could not get rate limits")
         sys.exit(1)
     keywords = [urllib.quote(k.encode('utf-8').replace('@', 'from:'),'') for k in keywords]
     queries = [" OR ".join(a) for a in chunkize(keywords, 3)]
@@ -81,11 +81,11 @@ def searcher(pile, searchco, keywords, debug=False):
                 next_reset += 15*60
                 left = max_per_reset
         if not left:
-            log("WARNING", "Stalling search queries with rate exceeded for the next %s seconds\n" % max(0, int(next_reset - time.time())))
+            log("WARNING", "Stalling search queries with rate exceeded for the next %s seconds" % max(0, int(next_reset - time.time())))
             time.sleep(timegap + max(0, next_reset - time.time()))
             continue
         if debug:
-            log("DEBUG", "Starting search queries with %d remaining calls for the next %s seconds\n" % (left, int(next_reset - time.time())))
+            log("DEBUG", "Starting search queries with %d remaining calls for the next %s seconds" % (left, int(next_reset - time.time())))
         for i, query in enumerate(queries):
             since = queries_since_id[i]
             max_id = 0
@@ -98,7 +98,7 @@ def searcher(pile, searchco, keywords, debug=False):
                 try:
                     res = searchco.search.tweets(**args)
                 except (TwitterHTTPError, BadStatusLine, URLError, SSLError) as e:
-                    log("WARNING", "Search connection could not be established, retrying in 2 secs (%s: %s)\n" % (type(e), e))
+                    log("WARNING", "Search connection could not be established, retrying in 2 secs (%s: %s)" % (type(e), e))
                     time.sleep(2)
                     continue
                 tweets = res.get('statuses', [])
@@ -106,7 +106,7 @@ def searcher(pile, searchco, keywords, debug=False):
                 if not len(tweets):
                     break
                 if debug:
-                    log("DEBUG", "[search] +%d tweets (%s)\n" % (len(tweets), query))
+                    log("DEBUG", "[search] +%d tweets (%s)" % (len(tweets), query))
                 for tw in tweets:
                     tid = long(tw.get('id_str', str(tw.get('id', ''))))
                     if not tid:
@@ -128,7 +128,7 @@ if __name__=='__main__':
         SearchConn = Twitter(domain="api.twitter.com", api_version="1.1", format="json", auth=oauth2, secure=True)
         StreamConn = TwitterStream(domain="stream.twitter.com", api_version="1.1", auth=oauth, secure=True, block=False, timeout=10)
     except Exception as e:
-        log('ERROR', 'Could not initiate connections to Twitter API: %s %s\n' % (type(e), e))
+        log('ERROR', 'Could not initiate connections to Twitter API: %s %s' % (type(e), e))
         sys.exit(1)
     try:
         db = pymongo.Connection(conf['mongo']['host'], conf['mongo']['port'])[conf['mongo']['db']]
@@ -136,7 +136,7 @@ if __name__=='__main__':
         coll.ensure_index([('_id', pymongo.ASCENDING)], background=True)
         coll.ensure_index([('timestamp', pymongo.ASCENDING)], background=True)
     except:
-        log('ERROR', 'Could not initiate connection to MongoDB\n')
+        log('ERROR', 'Could not initiate connection to MongoDB')
         sys.exit(1)
 
     pile = Queue()
