c51da44abafe2dd637af23c81451a3bb7218ffb2
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-08-14 11:24:48 +0200

    improve run script

--- restart.sh
+++ restart.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-pkill -f "python gazouilloire/run.py"
+cd "$(dirname "$0")"
+source $(which virtualenvwrapper.sh)
+deactivate > /dev/null 2>&1
+workon twitter
+pkill -9 -f "python gazouilloire/run.py"
 sleep 1
 touch runlog.txt
 mv -f runlog.txt runlog.txt.old
 python gazouilloire/run.py > runlog.txt 2>&1 &
+deactivate
 tail -f runlog.txt
-
