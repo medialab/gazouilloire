803dc95b3e35f3b34baf02f98507293446667818
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2015-01-15 16:45:36 +0100

    better handle multiinstances

--- restart.sh
+++ restart.sh
@@ -4,10 +4,13 @@ cd "$(dirname "$0")"
 source $(which virtualenvwrapper.sh)
 deactivate > /dev/null 2>&1
 workon gazouilloire
-pkill -9 -f "python gazouilloire/run.py"
-sleep 1
+if test -s gazouilloire.pid && ps -ef | grep "$(cat gazouilloire.pid)" | grep "python gazouilloire/run.py"; then
+  kill -9 "$(cat gazouilloire.pid)"
+  sleep 1
+fi
 touch runlog.txt
 mv -f runlog.txt runlog.txt.old
 python gazouilloire/run.py > runlog.txt 2>&1 &
+echo $! > gazouilloire.pid
 deactivate
 tail -f runlog.txt
