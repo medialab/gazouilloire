c89d6a4c4fa1007667c908469a04c24c5bde941c
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2015-01-15 12:24:34 +0100

    don't include breaklines in csv export

--- bin/export_csv.py
+++ bin/export_csv.py
@@ -14,7 +14,7 @@ print "url,user_screen_name,text,timestamp,lang,coordinates"
 for t in db.find(sort=[("_id", -1)]):
     ts = datetime.strptime(t['created_at'], '%a %b %d %H:%M:%S +0000 %Y').isoformat()
     coords = "::".join([str(a) for a in t["geo"]["coordinates"]]) if t["geo"] else ""
-    text = '"' + t["text"].replace('"', '""') + '"'
+    text = '"' + t["text"].replace('"', '""').replace("\n", " ") + '"'
     name = t.get("user_screen_name", t.get("user_name", ""))
     print ",".join([a.encode("utf-8") for a in [t["url"],name,text,ts,t["lang"],coords]])
 
