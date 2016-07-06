d67949ff33a476bff32a8193adf10b0c75edd851
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-07-06 16:21:09 +0200

    allow to give full mongodb query as argument of exporter

--- bin/export_csv_as_tcat.py
+++ bin/export_csv_as_tcat.py
@@ -134,14 +134,22 @@ keys = [
   "medias_urls",
   "medias_files"
 ]
-print ",".join(keys)
 query = {}
 if len(sys.argv) == 2:
-    query = {"text": re.compile(sys.argv[1].replace(' ', '\s+'), re.I)}
+    if '{' in sys.argv[1]:
+        try:
+            query = eval(sys.argv[1])
+        except Exception as e:
+            sys.stderr.write("WARNING: query wrongly formatted: %s\n" % sys.argv[1])
+            sys.exit("%s: %s\n" % (type(e), e))
+    else:
+        query = {"text": re.compile(sys.argv[1].replace(' ', '\s+'), re.I)}
 elif len(sys.argv) > 2:
     query["$or"] = []
     for arg in sys.argv[1:]:
         query["$or"].append({"text": re.compile(arg.replace(' ', '\s+'), re.I)})
+
+print ",".join(keys)
 for t in db.find(query, sort=[("_id", -1)]):
     print ",".join(format_csv(get_field(k, t)) for k in keys)
 
