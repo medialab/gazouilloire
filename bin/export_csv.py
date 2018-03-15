148c7de171793aae2d81f21b1f3ffb038c0305c6
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-03-15 11:46:30 +0100

    more fields in simple csv export

--- bin/export_csv.py
+++ bin/export_csv.py
@@ -10,12 +10,14 @@ with open('config.json') as confile:
 
 db = MongoClient(conf['mongo']['host'], conf['mongo']['port'])[conf['mongo']['db']]['tweets']
 
-print "url,user_screen_name,timestamp,lang,coordinates,text"
-for t in db.find(sort=[("_id", -1)]):
+print "url,user_screen_name,timestamp,user_lang,lang,coordinates,text,reply_to_url,is_retweet"
+for t in db.find({}, sort=[("_id", -1)]):
     ts = datetime.strptime(t['created_at'], '%a %b %d %H:%M:%S +0000 %Y').isoformat()
     coords = "::".join([str(a) for a in t["geo"]["coordinates"]]) if t["geo"] else ""
     text = '"' + t["text"].replace('"', '""').replace("\n", " ").replace("\r", "") + '"'
     name = t.get("user_screen_name", t.get("user_name", ""))
+    rt = "1" if text.startswith("RT @") else "0"
     url = t["url"] if "url" in t else "https://twitter.com/%s/status/%s" % (name, t["_id"])
-    print ",".join([a.encode("utf-8") for a in [url,name,ts,t["lang"],coords,text]])
+    reply = "" if not t.get("in_reply_to_status_id_str", "") else "https://twitter.com/%s/status/%s" % (t.get("in_reply_to_screen_name", ""), t.get("in_reply_to_status_id_str", ""))
+    print ",".join([a.encode("utf-8") for a in [url,name,ts,t.get("user_lang", ""),t.get("lang", ""),coords,text,reply,rt]])
 
