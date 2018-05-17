3c31441c562be119d07ab08fa2b46a45fd2ca35a
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-05-17 19:43:37 +0200

    add export script for top shared links

---
+++ bin/export_shared_links.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json, sys
+import progressbar
+from pymongo import MongoClient
+from gazouilloire.web.export import format_csv
+
+with open('config.json') as confile:
+    conf = json.loads(confile.read())
+
+db = MongoClient(conf['mongo']['host'], conf['mongo']['port'])[conf['mongo']['db']]['tweets']
+
+urls = {}
+query = {"$or": [{"lang": "fr"}, {"user_lang": "fr"}]}
+print "Counting matching results..."
+count = db.count(query)
+
+print "Querying and hashing results..."
+bar = progressbar.ProgressBar(max_value=count)
+for t in bar(db.find(query, limit=count, projection={"links": 1, "proper_links": 1})):
+    for l in t.get("proper_links", t["links"]):
+        if l not in urls:
+            urls[l] = 0
+        urls[l] += 1
+
+print "Storing json data..."
+with open("shared_urls.json", "w") as f:
+    json.dump(urls, f)
+
+print "Sorting and storing csv data..."
+with open("shared_urls.csv", "w") as f:
+    print >> f, "url,shares"
+    bar = progressbar.ProgressBar(max_value=len(urls))
+    for link, shares in bar(sorted(urls.items(), key = lambda x: -x[1])):
+        print >> f, '%s,%s' % (format_csv(link), shares)
