202cfb5a9237ff980d80998da9862ef07375f08c
farjasju <julesfarjas@gmail.com>
2018-10-19 11:07:39 +0200

    clean mongoconnection deprecated for complete_timestamps

--- bin/complete_timestamps.py
+++ bin/complete_timestamps.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import time, json, pymongo
+import time
+import json
+try:
+    from pymongo import MongoClient
+except ImportError:
+    from pymongo.connection import Connection as MongoClient
 
 with open('config.json') as confile:
     conf = json.loads(confile.read())
 
-db = pymongo.Connection(conf['mongo']['host'], conf['mongo']['port'])[conf['mongo']['db']]['tweets']
+db = MongoClient(conf['mongo']['host'], conf['mongo']['port'])[
+    conf['mongo']['db']]['tweets']
 
 for tweet in db.find():
     if "timestamp" not in tweet:
-        tweet['timestamp'] = time.mktime(time.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y'))
+        tweet['timestamp'] = time.mktime(time.strptime(
+            tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y'))
         db.save(tweet)
-
-
-
