6154f8095cb8fdea741410a469520cfaeece5537
farjasju <julesfarjas@gmail.com>
2018-11-07 17:36:50 +0100

    Handling tweets & links indices + adding find_todo() method

--- gazouilloire/database/elasticmanager.py
+++ gazouilloire/database/elasticmanager.py
@@ -4,33 +4,48 @@ import json
 import sys
 import os
 
+with open(os.path.dirname(os.path.realpath(__file__)) + '/db_settings.json', 'r') as db_settings:
+    DB_SETTINGS = json.loads(db_settings.read())
+
 
 class ElasticManager:
     def __init__(self, host, port, db):
-        self.host = host
-        self.port = port
-        self.index_name = db
-        self.es = Elasticsearch(host + ':' + str(port))
+        if db not in DB_SETTINGS['db_list']:
+            DB_SETTINGS['db_list'].append(db)
+            with open(os.path.dirname(os.path.realpath(__file__)) + '/db_settings.json', 'w') as db_settings:
+                json.dump(DB_SETTINGS, db_settings, indent=2)
+        else:
+            print('INFO -', "Using the existing '" + db + "'", 'database.')
+            self.host = host
+            self.port = port
+            self.db = Elasticsearch(host + ':' + str(port))
+            self.tweets = db + "_tweets"
+            self.links = db + "_links"
+
+    # main() methods
 
     def prepare_indices(self):
-        if not self.es.indices.exists(index=self.index_name):
+        if not self.db.indices.exists(index=self.tweets):
             try:
-                with open(os.path.dirname(os.path.realpath(__file__)) + '/elasticsearch_mapping.json') as mappingfile:
-                    mapping = json.loads(mappingfile.read())
-                    self.es.indices.create(index=self.index_name, body=mapping)
+                self.db.indices.create(
+                    index=self.tweets, body=DB_SETTINGS['tweets_mapping'])
+                self.db.indices.create(
+                    index=self.links, body=DB_SETTINGS['links_mapping'])
             except FileNotFoundError as e:
                 print(
-                    'ERROR -', 'Could not open elasticsearch_mapping.json: %s %s' % (type(e), e))
+                    'ERROR -', 'Could not open db_settings.json: %s %s' % (type(e), e))
                 sys.exit(1)
 
+    # depiler() methods
+
     def update(self, tweet_id,  new_value):
-        return self.es.update(index=self.index_name, doc_type='tweet', id=tweet_id, body={"doc": new_value, "doc_as_upsert": True})
+        return self.db.update(index=self.tweets, doc_type='tweet', id=tweet_id, body={"doc": new_value, "doc_as_upsert": True})
 
     def set_deleted(self, tweet_id):
-        return self.es.update(index=self.index_name, doc_type='tweet', id=tweet_id, body={"doc": {"deleted": True}, "doc_as_upsert": True})
+        return self.db.update(index=self.tweets, doc_type='tweet', id=tweet_id, body={"doc": {"deleted": True}, "doc_as_upsert": True})
 
     def find_one(self, tweet_id):
-        response = self.es.search(
+        response = self.db.search(
             index="tweets",
             body={
                 "query":
@@ -45,9 +60,44 @@ class ElasticManager:
             return None
         return response['hits']['hits'][0]['_source']
 
+    # resolver() methods
+
+    def find_todo(self, index):
+        # return index.find({"links_to_resolve": True}, projection={
+        #     "links": 1, "proper_links": 1, "retweet_id""retweet_id": 1}, limit=600, sort=[("_id", 1)])
+        response = self.db.search(
+            index=index,
+            body={
+                "_source": ["links", "proper_links", "retweet_id"],
+                "size": 600,
+                "sort": [
+                    {"_id": "asc"}
+                ],
+                "query":
+                    {
+                        "match": {
+                            "links_to_resolve": True
+                        }
+                }
+            }
+        )
+        if response['hits']['total'] == 0:
+            return None
+        result = []
+        for element in response['hits']['hits']:
+            # print("ELEMENT : ", element)
+            result_element = {}
+            result_element['_id'] = element['_id']
+            for key, value in element['_source'].items():
+                result_element[key] = value
+            result.append(result_element)
+        return result
+
 
 if __name__ == '__main__':
 
-    es = ElasticManager('localhost', 9200, 'tweets')
+    es = ElasticManager('localhost', 9200, 'test')
     es.prepare_indices()
-    print(es.find_one(1057589842425589760))
+    es.update(1059809701448290304,  {"links_to_resolve": True})
+    # print(es.find_todo(es.tweets))
+    print(len(es.find_todo(es.tweets)))
