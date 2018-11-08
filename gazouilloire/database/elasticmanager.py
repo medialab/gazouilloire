b7d54d191dafa837c8d0b82872d8bd0a79ae51ae
farjasju <julesfarjas@gmail.com>
2018-11-08 15:12:25 +0100

    Added 'find_already_resolved_links' method + some renaming & docstrings

--- gazouilloire/database/elasticmanager.py
+++ gazouilloire/database/elasticmanager.py
@@ -30,21 +30,26 @@ class ElasticManager:
     # main() methods
 
     def prepare_indices(self):
+        """Initializes the database"""
         if not self.db.indices.exists(index=self.tweets):
             self.db.indices.create(
                 index=self.tweets, body=DB_MAPPINGS['tweets_mapping'])
+        if not self.db.indices.exists(index=self.links):
             self.db.indices.create(
                 index=self.links, body=DB_MAPPINGS['links_mapping'])
 
     # depiler() methods
 
     def update(self, tweet_id,  new_value):
+        """Updates the given tweet to the content of 'new_value' argument"""
         return self.db.update(index=self.tweets, doc_type='tweet', id=tweet_id, body={"doc": new_value, "doc_as_upsert": True})
 
     def set_deleted(self, tweet_id):
+        """Sets the field 'deleted' of the given tweet to True"""
         return self.db.update(index=self.tweets, doc_type='tweet', id=tweet_id, body={"doc": {"deleted": True}, "doc_as_upsert": True})
 
-    def find_one(self, tweet_id):
+    def find_tweet(self, tweet_id):
+        """Returns the tweet corresponding to the given id"""
         response = self.db.search(
             index="tweets",
             body={
@@ -62,11 +67,12 @@ class ElasticManager:
 
     # resolver() methods
 
-    def find_todo(self, index):
+    def find_tweets_with_unresolved_tweets(self):
+        """Returns a list of tweets where 'links_to_resolve' field is True"""
         # return index.find({"links_to_resolve": True}, projection={
         #     "links": 1, "proper_links": 1, "retweet_id""retweet_id": 1}, limit=600, sort=[("_id", 1)])
         response = self.db.search(
-            index=index,
+            index=self.tweets,
             body={
                 "_source": ["links", "proper_links", "retweet_id"],
                 "size": 600,
@@ -93,11 +99,39 @@ class ElasticManager:
             result.append(result_element)
         return result
 
+    def find_already_resolved_links(self, urlstoclear):
+        """Returns a list of tweets which ids are in the 'urlstoclear' list argument"""
+        response = self.db.search(
+            index=self.links,
+            body={
+                "query":
+                    {
+                        "terms": {"_id": urlstoclear}
+                    }
+            }
+        )
+        if response['hits']['total'] == 0:
+            return None
+        result = []
+        for element in response['hits']['hits']:
+            # print("ELEMENT : ", element)
+            result_element = {}
+            result_element['_id'] = element['_id']
+            for key, value in element['_source'].items():
+                result_element[key] = value
+            result.append(result_element)
+        return result
+
 
 if __name__ == '__main__':
 
     es = ElasticManager('localhost', 9200, 'test')
     es.prepare_indices()
-    es.update(1059809701448290304,  {"links_to_resolve": True})
-    # print(es.find_todo(es.tweets))
-    print(len(es.find_todo(es.tweets)))
+    todo = es.find_tweets_with_unresolved_tweets()
+    print('>> todo : ', todo[:10])
+    urlstoclear = list(set([l for t in todo if not t.get(
+        "proper_links", []) for l in t.get('links', [])]))
+    print('>> urlstoclear : ', urlstoclear[:10])
+    alreadydone = [{l["_id"]: l["real"]
+                    for l in es.find_already_resolved_links(urlstoclear)}]
+    print('>> alreadydone : ', alreadydone[:10])
