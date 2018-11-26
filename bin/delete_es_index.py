1f0a072057496d5c2962b31cf1044a17999a23ec
farjasju <julesfarjas@gmail.com>
2018-11-26 16:17:34 +0100

    Adding a migration script (& an index deletion script)

---
+++ bin/delete_es_index.py
@@ -0,0 +1,32 @@
+# coding: utf8
+import click
+from elasticsearch import Elasticsearch
+from distutils.util import strtobool
+
+
+@click.command()
+@click.argument('es_host')
+@click.argument('es_port')
+@click.argument('es_index_name')
+def delete_index(es_host, es_port, es_index_name):
+    es = Elasticsearch('http://' + es_host + ':' + es_port)
+
+    tweets = es_index_name + '_tweets'
+    links = es_index_name + '_links'
+
+    if es.indices.exists(index=tweets):
+        choice1 = strtobool(input(
+            "Are you sure you want to delete " + tweets + "? (y/n) "))
+        if choice1:
+            es.indices.delete(index=tweets)
+        print(">", tweets, "successfully deleted.")
+    if es.indices.exists(index=links):
+        choice2 = strtobool(input(
+            "Are you sure you want to delete " + links + "? (y/n) "))
+        if choice2:
+            es.indices.delete(index=links)
+        print(">", links, "successfully deleted.")
+
+
+if __name__ == '__main__':
+    delete_index()
