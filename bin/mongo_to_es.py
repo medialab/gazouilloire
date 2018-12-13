a855ba3c91738f068909ed50ba506d0929775c29
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-12-13 16:32:51 +0100

    refacto mappings + clean unused imports

--- bin/mongo_to_es.py
+++ bin/mongo_to_es.py
@@ -3,30 +3,23 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-
-import json
+from builtins import input, int, open, str
 import os
 import sys
-from builtins import input, int, open, str
-from datetime import datetime, timedelta
-from distutils.util import strtobool
-from pprint import pprint
-
 import click
+from datetime import datetime
+from distutils.util import strtobool
+
 import pymongo
-import requests
 from elasticsearch import Elasticsearch, helpers
+
 from future import standard_library
 
+sys.path.append(os.path.join(os.getcwd()))
+
+from gazouilloire.database.elasticmanager import DB_MAPPINGS, format_tweet_fields
+
 standard_library.install_aliases()
-
-try:
-    with open(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'gazouilloire', 'database', 'db_mappings.json'), 'r') as db_mappings:
-        DB_MAPPINGS = json.loads(db_mappings.read())
-except FileNotFoundError as e:
-    print('ERROR -', 'Could not open db_mappings.json: %s %s' % (type(e), e))
-    sys.exit(1)
-
 pymongo.unicode_decode_output = False
 
 ES_TWEETS_MAPPINGS = DB_MAPPINGS['tweets_mapping']
@@ -80,70 +73,10 @@ def migrate(mongo_host, mongo_port, mongo_db, es_host, es_port, es_index_name):
     print("Migrating database...")
     for tweet in MONGO_DB.tweets.find():
         i += 1
-        try:
-            coordinates = tweet['coordinates']['coordinates']
-        except:
-            coordinates = tweet.get('coordinates', None)
-        load = {
+        bulkload.append({
             '_id': tweet['_id'],
-            '_source': {
-                "collected_at_timestamp": tweet.get('collected_at_timestamp', None),
-                "collected_via_search": tweet.get('collected_via_search', None),
-                "collected_via_stream": tweet.get('collected_via_stream', None),
-                "coordinates": coordinates,
-                "created_at": tweet.get('created_at', None),
-                "deleted": False,
-                "favorite_count": tweet.get('favorite_count', None),
-                "hashtags": tweet.get('hashtags', None),
-                "in_reply_to_screen_name": tweet.get('in_reply_to_screen_name', None),
-                "in_reply_to_status_id_str": tweet.get('in_reply_to_status_id_str', None),
-                "in_reply_to_user_id_str": tweet.get('in_reply_to_user_id_str', None),
-                "lang": tweet.get('lang', None),
-                "langs": tweet.get('langs', None),
-                "links": tweet.get('links', None),
-                "links_to_resolve": tweet.get('links_to_resolve', None),
-                "medias": tweet.get('medias', None),
-                "mentions_ids": tweet.get('mentions_ids', None),
-                "mentions_names": tweet.get('mentions_names', None),
-                "possibly_sensitive": tweet.get('possibly_sensitive', None),
-                "proper_links": tweet.get('proper_links', None),
-                "quoted_id": tweet.get('quoted_id', None),
-                "quoted_timestamp": tweet.get('quoted_timestamp', None),
-                "quoted_user": tweet.get('quoted_user', None),
-                "quoted_user_id": tweet.get('quoted_user_id', None),
-                "reply_count": tweet.get('reply_count', None),
-                "retweet_count": tweet.get('retweet_count', None),
-                "retweet_id": tweet.get('retweet_id', None),
-                "retweet_timestamp": tweet.get('retweet_timestamp', None),
-                "retweet_user": tweet.get('retweet_user', None),
-                "retweet_user_id": tweet.get('retweet_user_id', None),
-                "source": tweet.get('source', None),
-                "text": tweet.get('text', None),
-                "timestamp": tweet.get('timestamp', None),
-                "truncated": tweet.get('truncated', None),
-                "tweet_id": tweet.get('_id', None),
-                "url": tweet.get('url', None),
-                "user_created_at": tweet.get('user_created_at', None),
-                "user_created_at_timestamp": tweet.get('user_created_at_timestamp', None),
-                "user_description": tweet.get('user_description', None),
-                "user_favourites": tweet.get('user_favourites', None),
-                "user_followers": tweet.get('user_followers', None),
-                "user_friends": tweet.get('user_friends', None),
-                "user_id_str": tweet.get('user_id_str', None),
-                "user_lang": tweet.get('user_lang', None),
-                "user_listed": tweet.get('user_listed', None),
-                "user_location": tweet.get('user_location', None),
-                "user_name": tweet.get('user_name', None),
-                "user_profile_image_url": tweet.get('user_profile_image_url', None),
-                "user_profile_image_url_https": tweet.get('user_profile_image_url_https', None),
-                "user_screen_name": tweet.get('user_screen_name', None),
-                "user_statuses": tweet.get('user_statuses', None),
-                "user_time_zone": tweet.get('user_time_zone', None),
-                "user_url": tweet.get('user_url', None),
-                "user_utc_offset": tweet.get('user_utc_offset', None),
-                "user_verified": tweet.get('user_verified', None)}}
-
-        bulkload.append(load)
+            '_source': format_tweet_fields(tweet)
+        })
 
         if i % 1800 == 0:
             helpers.bulk(ES_CLIENT, bulkload,
@@ -156,14 +89,13 @@ def migrate(mongo_host, mongo_port, mongo_db, es_host, es_port, es_index_name):
     links_bulkload = []
     for link in MONGO_DB.links.find():
         i += 1
-
-        load = {
+        links_bulkload.append({
             '_source': {
                 "link_id": link['_id'],
                 "real": link.get('real', None)
-            }}
+            }
+        })
 
-        links_bulkload.append(load)
 
         if i % 1800 == 0:
             helpers.bulk(ES_CLIENT, links_bulkload,
