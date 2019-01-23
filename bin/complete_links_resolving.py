31237f76086b668a07fd3c41fa459e2b4a7f5cb3
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2019-01-23 18:02:39 +0100

    use links_resolving in collect_list_accounts + script to finalize resolving after stopped collection

--- bin/complete_links_resolving.py
+++ bin/complete_links_resolving.py
@@ -1,20 +1,18 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
+import json
 from pymongo import MongoClient, ASCENDING
 from fake_useragent import UserAgent
-from config import CSV_SOURCE, CSV_ENCODING, CSV_TWITTER_FIELD, MONGO_DATABASE, TWITTER
 
 from gazouilloire.run import resolve_url
-from gazouilloire.tweets import prepare_tweet, clean_user_entities
-from gazouilloire.api_wrapper import TwitterWrapper
 
-with open(CSV_SOURCE) as f:
-    data = list(csv.DictReader(f, delimiter=','))
+with open('config.json') as confile:
+    conf = json.loads(confile.read())
 
-api = TwitterWrapper(TWITTER)
-db = MongoClient("localhost", 27017)[MONGO_DATABASE]
+db = MongoClient(conf["mongo"]["host"], conf["mongo"]["port"])[conf["mongo"]["db"]]
+
+# TODO: refacto all of this with gazouilloire/run.py
 
 linkscoll = db['links']
 tweetscoll = db['tweets']
@@ -22,50 +20,6 @@ for f in ['retweet_id', 'in_reply_to_status_id_str', 'timestamp',
           'links_to_resolve', 'lang', 'user_lang', 'langs']:
     tweetscoll.ensure_index([(f, ASCENDING)], background=True)
 tweetscoll.ensure_index([('links_to_resolve', ASCENDING), ('_id', ASCENDING)], background=True)
-
-for i, row in enumerate(data):
-    user = {}
-    for k in row.keys():
-        user[k.decode(CSV_ENCODING)] = row[k].decode(CSV_ENCODING).replace(u' ', ' ').strip()
-    user['twitter'] = user[CSV_TWITTER_FIELD].lstrip('@').lower()
-    print "- WORKING ON %s" % user['twitter'], user
-    if db.users.find({'_id': user['twitter'], 'done': True}, limit=1).count():
-        print "  ALREADY DONE!"
-        continue
-    user['done'] = False
-    api_args = {'screen_name': user['twitter']}
-    metas = api.call('users.show', api_args)
-    if not metas:
-        print "SKIPPING tweets for %s whose account unfortunately disappeared" % user['twitter']
-        continue
-    clean_user_entities(metas)
-    user.update(metas)
-    user.pop('_id')
-    db.users.update({'_id': user['twitter']}, {"$set": user}, upsert=True)
-    if user['protected']:
-        print "SKIPPING tweets for %s whose account is unfortunately protected" % user['twitter']
-        continue
-    api_args['count'] = 200
-    api_args['contributor_details'] = 1
-    api_args['include_rts'] = 1
-    api_args['tweet_mode'] = "extended"
-    tweets = api.call('statuses.user_timeline', api_args)
-    while tweets:
-        for tw in tweets:
-            api_args['max_id'] = min(api_args.get('max_id', tw['id']), tw['id']-1)
-            metas = prepare_tweet(tw)
-            metas.pop('_id')
-            tw.update(metas)
-            for po in ['user', 'entities', 'extended_entities']:
-                if po in tw:
-                    tw.pop(po)
-            db.tweets.update({'_id': tw['id']}, {"$set": tw}, upsert=True)
-        print "...collected %s new tweets" % len(tweets)
-        tweets = api.call('statuses.user_timeline', api_args)
-    db.users.update({'_id': user['twitter']}, {"$set": {"done": True}})
-
-
-# TODO: refacto all of this with gazouilloire/run.py
 
 ua = UserAgent()
 ua.update()
