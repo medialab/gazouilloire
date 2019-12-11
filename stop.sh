7f3188850075b1eb0c41a8ba82f5baf45356e951
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2019-12-11 16:21:33 +0100

    optionnalize stop delay

--- stop.sh
+++ stop.sh
@@ -2,10 +2,15 @@
 
 cd "$(dirname "$0")"
 
+DELAY=$1
+if [ -z "$DELAY" ]; then
+  DELAY=300
+fi
+
 pkill -f "python gazouilloire/run.py $(pwd)"
-echo "waiting for previous process to terminate..."
+echo "waiting ${DELAY}s for previous process to terminate..."
 elapsed=0
-while ps -ef | grep -v "\(grep\|[0-9]  0 [0-9]\)" | grep "python gazouilloire/run.py $(pwd)" > /dev/null && [ $elapsed -lt 300 ]; do
+while ps -ef | grep -v "\(grep\|[0-9]  0 [0-9]\)" | grep "python gazouilloire/run.py $(pwd)" > /dev/null && [ $elapsed -lt $DELAY ]; do
   elapsed=$(($elapsed + 1))
   sleep 1
 done
