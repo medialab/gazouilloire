76221aabbc91634ab90003e9d8ec1c4f157f115c
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-04-14 12:24:18 +0200

    cleanup bad characters

--- bin/export_users_urls_domains.py
+++ bin/export_users_urls_domains.py
@@ -20,7 +20,7 @@ count = db.count(query)
 
 print "Building and storing csv data..."
 with open("users_urls_domains.csv", "w") as f:
-    print >> f, "user_screenname,link,domain,datetime,is_retweet"
+    print >> f, "user_screenname,url,normalized_url,domain,datetime,is_retweet"
     bar = progressbar.ProgressBar(max_value=count)
     for t in bar(db.find(query, limit=count, projection={"user_screen_name": 1, "links": 1, "proper_links": 1, "retweet_id": 1, "created_at": 1})):
         links = t.get("proper_links", t["links"])
@@ -30,10 +30,10 @@ with open("users_urls_domains.csv", "w") as f:
         isRT = 1 if t["retweet_id"] else 0
         dtime = isodate(t["created_at"])
         for l in links:
-            lnk = normalize_url(l)
+            lnk = normalize_url(l.replace("%0D", ""), strip_trailing_slash=True, strip_lang_subdomains=True)
             try:
                 domain = normalize_url(l.split("/")[2])
             except Exception as e:
                 print >> sys.stderr, "ERROR normalizing domain for url", l, type(e), e
                 domain = ""
-            print >> f, ",".join([format_csv(v) for v in [name, lnk, domain, dtime, str(isRT)]])
+            print >> f, ",".join([format_csv(v) for v in [name, l, lnk, domain, dtime, str(isRT)]])
