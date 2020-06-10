2ff4a994fc54ea41c44d32eeff10de1c08f33206
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-06-10 11:51:13 +0200

    more metas output in export users/urls

--- bin/export_users_urls_domains.py
+++ bin/export_users_urls_domains.py
@@ -20,14 +20,17 @@ count = db.count(query)
 
 print "Building and storing csv data..."
 with open("users_urls_domains.csv", "w") as f:
-    print >> f, "user_screenname,url,normalized_url,domain,datetime,is_retweet"
+    print >> f, "user_screenname,user_id,url,normalized_url,domain,datetime,is_retweet,followers,has_media"
     bar = progressbar.ProgressBar(max_value=count)
-    for t in bar(db.find(query, limit=count, projection={"user_screen_name": 1, "links": 1, "proper_links": 1, "retweet_id": 1, "created_at": 1})):
+    for t in bar(db.find(query, limit=count, projection={"user_screen_name": 1, "user_id_str": 1, "links": 1, "proper_links": 1, "retweet_id": 1, "created_at": 1, "user_followers": 1, "medias": 1})):
         links = t.get("proper_links", t["links"])
         if not links:
             continue
         name = t.get("user_screen_name")
+        uid = t.get("user_id_str")
         isRT = 1 if t["retweet_id"] else 0
+        fols = t["user_followers"]
+        media = 1 if t["medias"] else 0
         dtime = isodate(t["created_at"])
         for l in links:
             lnk = normalize_url(l.replace("%0D", ""), strip_trailing_slash=True, strip_lang_subdomains=True)
@@ -36,4 +39,4 @@ with open("users_urls_domains.csv", "w") as f:
             except Exception as e:
                 print >> sys.stderr, "ERROR normalizing domain for url", l, type(e), e
                 domain = ""
-            print >> f, ",".join([format_csv(v) for v in [name, l, lnk, domain, dtime, str(isRT)]])
+            print >> f, ",".join([format_csv(v) for v in [name, uid, l, lnk, domain, dtime, str(isRT), fols, str(media)]])
