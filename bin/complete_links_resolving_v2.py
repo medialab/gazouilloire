9750d4f4c1a6ffe6f9ed19fc57b2a484180f125b
bmaz <beatrice.mazoyer@gmail.com>
2020-09-24 17:42:50 +0200

    #38 replace db_conf argument by host, port, db_name in complete_link_resolving_v2

--- bin/complete_links_resolving_v2.py
+++ bin/complete_links_resolving_v2.py
@@ -14,10 +14,12 @@ with open('config.json') as confile:
 
 @click.command()
 @click.argument('batch_size', default=BATCH_SIZE)
-@click.argument('db_conf', default=conf["database"])
+@click.argument('host', default=conf["database"]["host"])
+@click.argument('port', default=conf["database"]["port"])
+@click.argument('db_name', default=conf["database"]["db_name"])
 @click.option('--verbose/--silent', default=False)
-def resolve_script(batch_size, db_conf, verbose=False):
-    db = prepare_db(**db_conf)
+def resolve_script(batch_size, host, port, db_name, verbose=False):
+    db = prepare_db(host, port, db_name)
     skip = 0
     todo = count_and_log(db, batch_size, skip=skip)
     while todo:
