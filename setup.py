96080d9e4006e594ec6b9b74c41a52e8995e4d1b
bmaz <beatrice.mazoyer@gmail.com>
2020-09-25 18:36:34 +0200

    #37 Declare module with setup.py file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('./README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='gazouilloire',
+      version='0.0.0',
+      description='Twitter stream & search API grabber',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/medialab/gazouilloire',
+      license='GPL-3.0',
+      author='Benjamin Ooghe-Tabanou',
+      author_email='',
+      keywords='twitter',
+      python_requires='>=3.6',
+      packages=find_packages(exclude=["collect*", "dist", "build"]),
+      include_package_data=True,
+      install_requires=[
+          "elasticsearch>=6.0.0, <7.0.0",
+          "pymongo >= 3.0",
+          "twitter >= 1.14.1",
+          "requests",
+          "urllib3[secure]",
+          "pytz",
+          "ural",
+          "minet >= 0.20.2",
+          "future"
+      ]
+      )
