15a49deff215c7096f1386342f479b35bffac604
bmaz <beatrice.mazoyer@gmail.com>
2020-09-29 17:13:12 +0200

    #37 create gazouilloire init command

--- gazouilloire/cli/__main__.py
+++ gazouilloire/cli/__main__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import click
+from gazouilloire.config import create_conf_example, load_conf
 
 @click.group()
 def main():
@@ -9,14 +10,17 @@ def main():
 @main.command()
 @click.argument('path', type=click.Path(exists=True), default=".")
 def init(path):
-    click.echo("init " + path)
+    create_conf_example(path)
 
 
 @main.command()
-def run():
-    click.echo("run")
+@click.argument('path', type=click.Path(exists=True), default=".")
+def run(path):
+    conf = load_conf(path)
+    raise NotImplementedError
 
 
 @main.command()
 def resolve():
     click.echo("resolve")
+    raise NotImplementedError
