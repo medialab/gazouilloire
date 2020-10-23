105a2c7f0094b2add50bfe9b0d0cb2afc5680c52
bmaz <beatrice.mazoyer@gmail.com>
2020-10-23 11:30:01 +0200

    remove encode(utf-8) for python3

--- gazouilloire/web/export.py
+++ gazouilloire/web/export.py
@@ -263,7 +263,7 @@ def get_thread_ids_from_query(query, mongocoll):
 
 def yield_csv(queryiterator, list_fields=TWEET_FIELDS, extra_fields=[]):
     out_fields = list_fields + extra_fields
-    yield ",".join(out_fields).encode('utf-8')
+    yield ",".join(out_fields)
     for t in queryiterator:
         source = t["_source"]
         source["_id"] = t["_id"]
