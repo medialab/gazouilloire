d571cb386d0ee9bc4625ab790426e31438faa1e6
bmaz <beatrice.mazoyer@gmail.com>
2021-01-08 19:33:27 +0100

    version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='gazouilloire',
-      version='2.0.1a3  ',
+      version='2.0.0-alpha',
       description='Twitter stream & search API grabber',
       long_description=long_description,
       long_description_content_type='text/markdown',
