cafa911dec5b96e5c75288b811f8f07a84979fa6
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2021-09-20 17:19:34 +0200

    add to restart script logic to try to automatically restart ES if it was down (closes #100)

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -3,10 +3,20 @@
 # - Usage:
 # Place this script in your gazouilloire corpus directory
 # Run it by giving it the corpus name as argument, for instance:
-# ./restart.sh mycorpus
+#   ./restart.sh mycorpus
 # It will automatically try to use pyenv to activate a virtualenv
 # named 'gazou-mycorpus' but you can force another env name like this:
-# ./restart.sh mycorpus myenv
+#   ./restart.sh mycorpus myenv
+# If a third argument is given as an integer, ElasticSearch will be tested
+# before restarting gazouilloire. If ES is down, the script will try to 
+# restart it for this number of seconds. For instance:
+#   ./restart.sh mycorpus myenv 60
+# sudo rights with no password need to be given to the user running this script
+# for the specific ES restart command for this option to work properly.
+# For instance by adding a file to /etc/sudoers.d/:
+#   sudo visudo -f /etc/sudoers.d/gazouilloire 
+# Then adding a line similar to the following within it:
+#   username ALL=(ALL) NOPASSWD: /usr/sbin/service elasticsearch restart
 #
 # - Prerequisites:
 # This script supposes gazouilloire was installed within a python environment using PyEnv:
@@ -18,8 +28,8 @@
 # So a typical crontab would look something like the following:
 #
 # m  h dom mon dow   command
-# @reboot            bash /data/gazouilloire/restart.sh CORPUSNAME
-# 00 4  *   *   *    bash /data/gazouilloire/restart.sh CORPUSNAME
+# @reboot            bash /data/gazouilloire/restart.sh CORPUSNAME CORPUSENV TIMEOUT_ES
+# 00 4  *   *   *    bash /data/gazouilloire/restart.sh CORPUSNAME CORPUSENV TIMEOUT_ES
 
 
 # Corpus name as argument 
@@ -29,6 +39,11 @@ CORPUS=$1
 CORPUSENV=$2
 if [ -z "$2" ]; then
   CORPUSENV="gazou-$CORPUS"
+fi
+
+TIMEOUT=0
+if [ ! -z "$3" ]; then
+  TIMEOUT=$3
 fi
 
 # Corpus directory assumed to be the same as the script's
@@ -42,6 +57,16 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 pyenv activate "$CORPUSENV"
+
+# Check collection's status to ensure ElasticSearch is running and reboot it when possible
+NOW=$(date +"%s")
+WAIT_UNTIL=$(($NOW + $TIMEOUT))
+while [ "$NOW" -lt "$WAIT_UNTIL" ] && gazou status "$CORPUSDIR" 2>&1 | grep "Connection to Elasticsearch failed."; do
+  echo "ElasticSearch seems to be down, trying to restart it..."
+  sudo service elasticsearch restart
+  sleep 2
+  NOW=$(date +"%s")
+done
 
 # Restart the corpus after waiting 5 minutes for the previous process to shut down cleanly
 gazou restart "$CORPUSDIR" --timeout 300
